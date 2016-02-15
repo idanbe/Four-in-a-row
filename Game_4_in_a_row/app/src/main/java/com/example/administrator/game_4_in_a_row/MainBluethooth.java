9608992c2814af2559ca571a2ee0f60aaa810f1a
idanbe <idan_beck@walla.com>
2016-02-15 13:26:57 +0200

    add bluetooth classe

---
+++ Game_4_in_a_row/app/src/main/java/com/example/administrator/game_4_in_a_row/MainBluethooth.java
@@ -0,0 +1,68 @@
+package com.example.administrator.game_4_in_a_row;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ViewAnimator;
+import android.util.Log;
+
+
+/**
+ * A simple launcher activity containing a summary sample description, sample log and a custom
+ * {@link android.support.v4.app.Fragment} which can display a view.
+ * <p>
+ * For devices with displays with a width of 720dp or greater, the sample log is always visible,
+ * on other devices it's visibility is controlled by an item on the Action Bar.
+ */
+
+
+public class MainBluethooth extends AppCompatActivity {
+
+        public static final String TAG = "MainActivity";
+
+        // Whether the Log Fragment is currently shown
+        private boolean mLogShown;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            if (savedInstanceState == null) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                BluetoothChatFragment fragment = new BluetoothChatFragment();
+                transaction.replace(R.id.sample_content_fragment, fragment);
+                transaction.commit();
+            }
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareOptionsMenu(Menu menu) {
+            //MenuItem logToggle = menu.findItem(R.id.menu_toggle_log);
+            //logToggle.setVisible(findViewById(R.id.sample_output) instanceof ViewAnimator);
+           // logToggle.setTitle(mLogShown ? R.string.sample_hide_log : R.string.sample_show_log);
+
+            return super.onPrepareOptionsMenu(menu);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            switch(item.getItemId()) {
+
+            }
+            return super.onOptionsItemSelected(item);
+        }
+
+
+    }
